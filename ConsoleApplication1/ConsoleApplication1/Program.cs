804611364489919f8becf3a36f799d159ebd29e0
kelund <kelund@lab7-16>
2016-08-31 15:34:15 -0600

    branch

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellos");
+            Console.WriteLine("Hello branch2");
         }
     }
 }
